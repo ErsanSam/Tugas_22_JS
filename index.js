6abb8ecbcf43e15da558b94dcd4551dca0162195
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-10 11:29:14 +0700

    Tugas 22 JS
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,8 @@
+function perulanganForEach(){
+  var tugas = ['Saya','ingin','belajar','bersama']
+  tugas.forEach(function(item,index){
+    console.log(item)
+    console.log(index)
+  })
+}
+perulanganForEach()
